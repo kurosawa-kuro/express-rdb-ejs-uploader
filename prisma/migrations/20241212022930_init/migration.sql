13db2f795e0f1851d6921add839b9ba25446afc5
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-13 10:43:46 +0900

    Remove Zone.Identifier files and associated migration SQL scripts

--- prisma/migrations/20241212022930_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `imagePath` on the `Micropost` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Micropost" DROP COLUMN "imagePath",
-ADD COLUMN     "imageUrl" TEXT;
